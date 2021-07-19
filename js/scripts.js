ce7a484a840d6f820ff0543bdff396fc6c3f8715
liujingjing618 <86307426+liujingjing618@users.noreply.github.com>
2021-07-19 15:29:57 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-/* Create a hambuyger menu with jQuery */
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-     x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
